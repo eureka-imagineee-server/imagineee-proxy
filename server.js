8f2cb2ab3e91bd6053f290e568730ba7a6b229f4
Imagineee <imagineeelego@gmail.com>
2021-07-09 17:37:32 +0100

    added scripts

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const request = require('request');
+const { v4, stringify, v5 } = require('uuid');
+const url = require('url');
+
+const app = express();
+
+var curUUID = {}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/v1/proxy/get/:url', (req, res) => {
+	if (req.query.uuid == curUUID[req.query.key].uuid) {
+		if (curUUID[req.params.key].uses <= 100) {
+			request(
+				{ url: 'https://' + req.parms.url },
+				(error, response, body) => {
+					if (error || response.statusCode !== 200) {
+						return res.status(500).json({ type: 'error', message: err.message });
+					}
+					res.json(JSON.parse(body));
+				}
+			)
+			curUUID[req.params.key].uses++
+		}
+		if (curCertificate[req.params.key].uses > 100) {
+			delete curUUID[req.params.key]
+		}
+	}
+})
+app.get('/v1/keys/get', (req, res) => {
+	let certs = {uuid: v4(), key: Math.floor(Math.random()*99999)*10000}
+	res.json(certs)
+	curUUID[certs.key] = {uuid: v4(), key: Math.floor(Math.random()*99999)*10000}
+	curUUID[certs.key].uses = 0
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+function fullUrl(req) {
+	return url.format({
+		protocol: req.protocol,
+		host: req.get('host'),
+		pathname: req.originalUrl
+	});
+}
