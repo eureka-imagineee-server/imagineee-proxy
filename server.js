b4dd29bcea79acd2d3eec5facfdff6b8a3e2a20d
Imagineee <imagineeelego@gmail.com>
2021-08-12 10:13:48 +0100

    added proxy script

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const express = require('express');
-const request = require('request');
-const { v4, stringify, v5 } = require('uuid');
-const url = require('url');
+const fetch = require('node-fetch');
 
 const app = express();
 
-var curUUID = {}
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-app.get('/v1/proxy/get/:url', (req, res) => {
-	if (req.query.uuid == curUUID[req.query.key].uuid) {
-		if (curUUID[req.params.key].uses <= 100) {
-			request(
-				{ url: 'https://' + req.parms.url },
-				(error, response, body) => {
-					if (error || response.statusCode !== 200) {
-						return res.status(500).json({ type: 'error', message: err.message });
-					}
-					res.json(JSON.parse(body));
-				}
-			)
-			curUUID[req.params.key].uses++
-		}
-		if (curCertificate[req.params.key].uses > 100) {
-			delete curUUID[req.params.key]
-		}
-	}
+app.get('/v1/proxy/get', (req, res) =>{
+  if (req.query.type == "text") {
+    fetch(req.query.url, {method: "GET"})
+    .then(res => res.text())
+    .then(body => res.json({text: body}));
+  } else if (req.query.type == "json") {
+    fetch(req.query.url, {method: "GET"})
+    .then(res => res.json())
+    .then(json => res.json({json: json}));
+  } else if (req.query.type == "blob") {
+    fetch(req.query.url, {method: "GET"})
+    .then(res => res.blob())
+    .then(blob => res.json({blob: blob}));
+  } else if (req.query.type == "arrayBuffer") {
+    fetch(req.query.url, {method: "GET"})
+    .then(res => res.arrayBuffer())
+    .then(arrayBuffer => res.json({arrayBuffer: arrayBuffer}));
+  } else {
+    fetch(req.query.url, {method: "GET"})
+    .then(res => res.text())
+    .then(body => res.json({text: body}));
+  }
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
+
